c173c23b2a1703d4766a6a3c8b75b0889998cd97
Adam Williams <30046495+adamllwilliams@users.noreply.github.com>
2026-03-09 17:08:39 +0000

    make cards collapsable

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,21 @@
 <x-layout>
     <div class="max-w-2xl mx-auto">
         @forelse ($events as $event)
-            <div class="card bg-base-100 shadow mt-8">
-                <div class="card-body">
-                    <div>
-                        <div class="font-semibold">{{ $event->name }}</div>
-                        <div class="mt-1">{{ $event->description }}</div>
-                        <div class="text-sm text-gray-500 mt-2">
-                            {{ $event->created_at->diffForHumans() }}
-                        </div>
+            <div class="collapse collapse-arrow bg-base-100 shadow mt-8">
+                <input type="checkbox" />
+                <div class="collapse-title">
+                    <div class="font-semibold">{{ $event->name }}</div>
+                    <div class="mt-1">{{ $event->description }}</div>
+                    <div class="text-sm text-gray-500 mt-2">
+                        {{ $event->created_at->diffForHumans() }}
                     </div>
+                </div>
+                <div class="collapse-content">
+                    <div class="mt-1">Hi</div>
                 </div>
             </div>
         @empty
-            <p class="text-gray-500">No events yet.</p>
+            <p class="text-gray-500">No events yet. Be the first to post!</p>
         @endforelse
     </div>
 </x-layout>
