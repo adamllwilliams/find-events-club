bfbe180680ce86616bb1674aa00ce056262196cc
Adam Williams <30046495+adamllwilliams@users.noreply.github.com>
2026-03-09 12:51:14 +0000

    update routing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,6 @@
-
-<!DOCTYPE html>
-<html lang="en" data-theme="lofi">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Find Events Club</title>
-    <link rel="preconnect" href="<https://fonts.bunny.net>">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-
-<body class="min-h-screen flex flex-col bg-base-200 font-sans">
-    <nav class="navbar bg-base-100">
-        <div class="navbar-start">
-            <a href="/" class="btn btn-ghost text-xl">findevents.club</a>
-        </div>
-    </nav>
-
-    <main class="flex-1 container mx-auto px-4 py-8">
-        <div class="max-w-2xl mx-auto">
+<x-layout>
+    <div class="max-w-2xl mx-auto">
+        {{-- @foreach ($events as $event) --}}
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
                     <div>
@@ -31,8 +10,6 @@
                     </div>
                 </div>
             </div>
-        </div>
-    </main>
-</body>
-
-</html>
+        {{-- @endforeach --}}
+    </div>
+</x-layout>
