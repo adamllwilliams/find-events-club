ba5a4e1c0e3e94d6693d141c411a2c8042779de5
Adam Williams <30046495+adamllwilliams@users.noreply.github.com>
2026-03-09 11:50:26 +0000

    create events table

---
+++ database/migrations/2026_03_09_112812_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->dateTime('start_datetime');
+            $table->dateTime('end_datetime');
+            $table->string('venue_name')->nullable();
+            $table->enum('status', ['pending','approved', 'rejected'])->default('pending');
+            $table->string('image_link')->nullable();
+            $table->string('ticket_link')->nullable();
+            $table->string('social_link')->nullable();
+            $table->string('organiser')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
