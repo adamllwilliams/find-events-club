55e410e6d8371c9dacf9a133ea0adea7286bed63
Adam Williams <30046495+adamllwilliams@users.noreply.github.com>
2026-03-10 13:50:15 +0000

    create feature test to validate order of events in events feed

--- tests/Feature/PageHomeTest.php
+++ tests/Feature/PageHomeTest.php
@@ -5,30 +5,39 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
 
-it('shows events feed placeholder', function (): void {
-    $this->get('/')
-        ->assertSeeText('No events yet. Be the first to post!');
-});
+it('shows events feed placeholder', function (): void
+    {
+        $this->get('/')
+            ->assertSeeText('No events yet. Be the first to post!');
+    });
 
-it('shows events feed', function (): void {
-    $events = [
-        ['name'=> '🎭 Comedy Show', 'description' => 'Stand-up comedy with top Cardiff comedians'],
-        ['name'=> '🎨 Art Exhibition', 'description' => 'Opening night - free drinks!'],
-        ['name'=> '🍺 Pub Quiz', 'description' => 'Weekly quiz night, £2 entry'],
-    ];
-    // Arrange
-    Event::factory()->create($events[0]);
-    Event::factory()->create($events[1]);
-    Event::factory()->create($events[2]);
+it('shows events feed', function (): void
+    {
+        $events = [
+            ['name'=> '🎭 Comedy Show', 'description' => 'Stand-up comedy with top Cardiff comedians'],
+            ['name'=> '🎨 Art Exhibition', 'description' => 'Opening night - free drinks!'],
+            ['name'=> '🍺 Pub Quiz', 'description' => 'Weekly quiz night, £2 entry'],
+        ];
+        // Arrange
+        Event::factory()->create($events[0]);
+        Event::factory()->create($events[1]);
+        Event::factory()->create($events[2]);
 
-    // Act & Assert
-    $this->get('/')
-        ->assertSeeText([
-            $events[0]['name'],
-            $events[0]['description'],
-            $events[1]['name'],
-            $events[1]['description'],
-            $events[2]['name'],
-            $events[2]['description'],
-            ]);
-});
+        // Act & Assert
+        $this->get('/')
+            ->assertSeeText([
+                $events[0]['name'],
+                $events[0]['description'],
+                $events[1]['name'],
+                $events[1]['description'],
+                $events[2]['name'],
+                $events[2]['description'],
+                ]);
+    });
+
+it('shows events by latest first', function (): void
+    {
+        // Arrange
+        // Act
+        // Assert
+    });
