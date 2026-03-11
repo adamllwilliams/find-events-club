20e4d17dc531806a0c8d962f73ed43c79bb97369
Adam Williams <30046495+adamllwilliams@users.noreply.github.com>
2026-03-11 18:25:48 +0000

    event index format

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,17 @@
                 <input type="checkbox" id="event-{{ $event->id }}" />
                 <label for="event-{{ $event->id }}" class="collapse-title cursor-pointer">
                     <div class="font-semibold text-base-content">{{ $event->name }}</div>
-                    <div class="mt-1 text-base-content">{{ $event->description }}</div>
+                    <div class="mt-1 text-base-content">
+                        Fri 27 Mar 19:30 - 22:00
+                        <br>
+                        Clwb Ifor Bach
+                    </div>
                     <div class="text-sm text-base-content/50 mt-2">
                         Posted {{ $event->created_at->diffForHumans() }}
                     </div>
                 </label>
                 <div class="collapse-content">
-                    <div class="mt-1 text-base-content">Fr 27 Mar 19:30 - 22:00 • Clwb Ifor Bach</div>
+                    <div class="mt-1 text-base-content text-sm">{{ $event->description }}</div>
                 </div>
             </div>
         @empty
